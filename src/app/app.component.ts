b4ca88d5fcd0baeb9fe07cb4e1dfbb61a366ad75
Jorgen Skogmo <js@dearstudio.dk>
2024-10-04 16:08:17 +0200

    seed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: "app-root",
+	standalone: true,
+	imports: [RouterOutlet],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.css",
+	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {
-  title = 'wishlist';
+	title = "wishlist";
 }
